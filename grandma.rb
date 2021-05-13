81f084158ea23320b7e4ea23e002149561d2e0e6
Shasha Yan <shashayan@gmail.com>
2021-05-13 03:01:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,14 +7,14 @@ def speak_to_grandma(phrase)
   else
     return "NO, NOT SINCE 1938!"
   end
-end 
+end
   # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
